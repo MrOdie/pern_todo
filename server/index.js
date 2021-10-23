b2815a8ecbd724b165c66859d4e2fa50c6db11a3
Sam ODonnell <sodonnell5214@gmail.com>
2021-10-22 20:58:36 -0400

    Finished basic API

--- server/index.js
+++ server/index.js
@@ -25,12 +25,54 @@ app.post("/todos", async(req, res) => {
 })
 
 // get all todos
+app.get("/todos", async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
 
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 // get a todo
+app.get("/todos/:id", async(req, res) => {
+    try {
+        console.log(req.params);
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
+        res.json(todo.rows);
+    } catch (error) {
+        console.error(err.message);
+    }
+})
 
 // update a todo
+app.put("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+
+        res.json(updateTodo);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 // delete a todo
+app.delete("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+
+        res.json(deleteTodo);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
